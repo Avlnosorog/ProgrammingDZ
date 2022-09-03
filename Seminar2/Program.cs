b2208af559aed9b23b4003d59cfb9f71df267b3a
Alexander <nosorog-2010@yandex.ru>
2022-09-03 22:11:20 +0300

    Save 005

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -25,12 +25,33 @@
 
 // Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа
 
-int number = new Random().Next(100, 1000);
+// int number = new Random().Next(100, 1000);
 
-//Console.WriteLine( "Введите трёхзначное число" );При желании можно ввести трёхзначное числол самому
-//int number = Convert.ToInt32(Console.ReadLine());
+// //Console.WriteLine( "Введите трёхзначное число" );При желании можно ввести трёхзначное числол самому
+// //int number = Convert.ToInt32(Console.ReadLine());
 
-int number1 = number/100;// Находим первую цифру трёхзначного числа
-int number2 = number%10;// Находим третью цифру нашего трёзначного числа
+// int number1 = number/100;// Находим первую цифру трёхзначного числа
+// int number2 = number%10;// Находим третью цифру нашего трёзначного числа
 
-Console.WriteLine(number+ "=>" +number1+ number2);
+// Console.WriteLine(number+ "=>" +number1+ number2);
+
+//_______________________________________________________________________________________________________________
+
+//Определить кратное ли второе  число относительно первого
+
+Console.WriteLine( "Введите число 1" );
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine( "Введите число 2" );
+int number2= Convert.ToInt32(Console.ReadLine());
+
+int result = number1 % number2;// int result при остатке от деления, если остатка нет, то число целое
+
+if(result == 0)// ели result приравнивается к нуля, т.е. остатка нет, значит вывд такой
+{
+    Console.WriteLine("Второе число кратное первому");
+}
+else
+{
+    Console.WriteLine("Второе число некратное первому");
+}
