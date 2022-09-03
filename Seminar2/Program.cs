f0a4b5904dc68bd8c49f27659a643644c2c325df
Alexander <nosorog-2010@yandex.ru>
2022-09-03 21:26:50 +0300

    Save 002

---
+++ Seminar2/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int number = new Random().Next(10, 100);
+
+int number1 = number/10;// при делении на десять получаем первое число от нашего двухзначного числа
+int number2 = number%10;// при остатке (%) при делении на 10, получаем первое число от нашего двухзачного числа
+
+//Console.WriteLine(number); запускаем для проверки
+//Console.WriteLine(number1);
+//Console.WriteLine(number2);
+
+Console.WriteLine(number);
+
+if(number1>number2)
+{
+    Console.WriteLine(number1+ ">" +number2);
+}
+else
+{
+    Console.WriteLine(number2+ ">" +number1);
+
+}
