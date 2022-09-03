3162f7d0e4b8a548af08c261d3348a9ae58e60ae
Alexander <nosorog-2010@yandex.ru>
2022-09-04 00:01:42 +0300

    Save 011

--- Seminar2_DZ/Program.cs
+++ Seminar2_DZ/Program.cs
@@ -1,8 +1,8 @@
-﻿//Напишите программу, которая на вход принимает трёхзначное число и на выходе показывает вторую цифру этого числа
+﻿// //Напишите программу, которая на вход принимает трёхзначное число и на выходе показывает вторую цифру этого числа
 
-int number = new Random().Next(100, 1000);
+// int number = new Random().Next(100, 1000);
 
-int number1 = number / 10;
-int number2 = number1 % 10;
+// int number1 = number / 10;
+// int number2 = number1 % 10;
 
-Console.WriteLine(number+ " -> " +number2);
+// Console.WriteLine(number+ " -> " +number2);
