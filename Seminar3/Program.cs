2506de2b4699cda529ff00d940c09912c078f182
Alexander <nosorog-2010@yandex.ru>
2022-09-05 02:42:43 +0300

    Save 001

---
+++ Seminar3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Console.WriteLine( "Введите число " );
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int count=number;
+
+// while(count>1000)
+//     {
+//       count/=10;
+//       //Console.WriteLine(count);
+//     }
+// Console.WriteLine(count%10);
+//_______________________________________________________________________________________________________________
+
+//Задача 2.(1) Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+
+Console.WriteLine( "Введите число " );
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number<100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+while( number > 1000)
+
+    {
+        number/=10;
+        
+    }
+
+Console.WriteLine(number%10);
+}
