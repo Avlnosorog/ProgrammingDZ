d79cd26df1d2babf663704a351573d758c5bb06e
Alexander <nosorog-2010@yandex.ru>
2022-09-05 23:14:04 +0300

    Save 006

--- Seminar3_DZ/Program.cs
+++ Seminar3_DZ/Program.cs
@@ -11,19 +11,28 @@
 // Console.WriteLine( "Введите пятизначное число " );
 // int number = Convert.ToInt32(Console.ReadLine());
 
-// int n=number/10000;
-// int n1=number/1000%10;
-// int n2=number/10%10;
-// int n3=number%10;
+//     int n=number/10000;
+//     int n1=number/1000%10;
+//     int n2=number/10%10;
+//     int n3=number%10;
 
-// if(n==n3 &&  n1==n2)
+// if(number>=10000 && number<100000)
 // {
-//     Console.WriteLine("Число " +number+ " является палиндромом ");
+//     if(n==n3 &&  n1==n2)
+//     {
+   
+//         Console.WriteLine("Число " +number+ " является палиндромом ");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Число " +number+ "  не является палиндромом ");
+//     }
 // }
 // else
 // {
-//     Console.WriteLine("Число " +number+ "  не является палиндромом ");
+//     Console.WriteLine("Это не пятизначное число");
 // }
+        
 //_____________________________________________________________________________________________________________
 //
 //Задача 21
