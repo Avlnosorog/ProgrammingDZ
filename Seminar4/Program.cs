c12b001f3d672259786119847752ac27498808c9
Alexander <nosorog-2010@yandex.ru>
2022-09-08 08:17:41 +0300

    Save

---
+++ Seminar4/Program.cs
@@ -0,0 +1,18 @@
+﻿// //Напишите программу. которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+// Console.WriteLine( "Введите число " );
+// int number = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Sum(number));
+
+// int Sum(int number)
+// {
+//     int sum = 0;
+//     int count = 1;
+//     while(count<=number)
+//     {
+//         sum = sum + count;
+//         count++;
+
+//     }
+//     return sum;
+// }
