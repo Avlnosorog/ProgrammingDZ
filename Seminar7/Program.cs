f408da7c078543f7dd28da8a95e2554317170430
Alexander <nosorog-2010@yandex.ru>
2022-09-21 02:07:04 +0300

    Save1

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -75,46 +75,46 @@
 // int rovs = Convert.ToInt32(Console.ReadLine());
 // int columns = Convert.ToInt32(Console.ReadLine());
 
-// int[,] matrix = new int [rovs, columns];
+// int[,] matrix = new int[rovs, columns];
 
 // void Matrix(int rovs, int columns)
 // {
-// for(int i = 0; i < matrix.GetLength(0); i++)
-// {                                                 // для двумерных массивов используют цикл for, а вместо Length
-//    for(int j = 0; j < matrix.GetLength(1); j++)   // применяют GetLength. for(int j = 0; j < matrix.GetLength(1); i++)                                  
-//    {
-//    matrix[i, j] =new Random().Next(3,4); 
-//    // matrix[i, j] = rovs+columns;                // Получается матрица с фиксированными элементами.
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {                                                 // для двумерных массивов используют цикл for, а вместо Length
+//         for (int j = 0; j < matrix.GetLength(1); j++)   // применяют GetLength. for(int j = 0; j < matrix.GetLength(1); i++)                                  
+//         {
+//             matrix[i, j] = new Random().Next(3, 4);
+//             // matrix[i, j] = rovs+columns;                // Получается матрица с фиксированными элементами.
 
-//    //Console.Write(matrix[i, j]+ "\t");            // чтобы получить таблицу, нужно использовать оператор Write
-//    Console.Write(matrix[i, j]+ " ");               // вместо WriteLine.
-//    }                                               // Вместо пробела можно использовать литерал \t, пример 
-//                                                    // Console.Write(matrix[i, j]+ "\t"); 
-//    Console.WriteLine();
+//             //Console.Write(matrix[i, j]+ "\t");            // чтобы получить таблицу, нужно использовать оператор Write
+//             Console.Write(matrix[i, j] + " ");               // вместо WriteLine.
+//         }                                               // Вместо пробела можно использовать литерал \t, пример 
+//                                                         // Console.Write(matrix[i, j]+ "\t"); 
+//         Console.WriteLine();
+//     }
 // }
-// }
-// Matrix( rovs, columns);
+// Matrix(rovs, columns);
 
 // void MatrixSquare(int rovs, int columns)
 // {
-// for(int i = 0; i < matrix.GetLength(0); i++)
-// {                                                 // для двумерных массивов используют цикл for, а вместо Length
-//    for(int j = 0; j < matrix.GetLength(1); j++)   // применяют GetLength. for(int j = 0; j < matrix.GetLength(1); i++)                                  
-//    {
-//    matrix[i, j] =new Random().Next(3,4); 
-//    {
-//    if(i%2==0 && j%2==0)
-//    {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {                                                 // для двумерных массивов используют цикл for, а вместо Length
+//         for (int j = 0; j < matrix.GetLength(1); j++)   // применяют GetLength. for(int j = 0; j < matrix.GetLength(1); i++)                                  
+//         {
+//             matrix[i, j] = new Random().Next(3, 4);
+//             {
+//                 if (i % 2 == 0 && j % 2 == 0)
+//                 {
 
-//    matrix[i, j]=matrix[i,j]*matrix[i,j];
-   
-//    }
-//                                                  // чтобы получить таблицу, нужно использовать оператор Write
-//    Console.Write(matrix[i, j]+ " ");               // вместо WriteLine.
-//    }                                               // Вместо пробела можно использовать литерал \t, пример 
-// }                                               // Console.Write(matrix[i, j]+ "\t"); 
-//    Console.WriteLine();
-// }
+//                     matrix[i, j] = matrix[i, j] * matrix[i, j];
+
+//                 }
+//                 // чтобы получить таблицу, нужно использовать оператор Write
+//                 Console.Write(matrix[i, j] + " ");               // вместо WriteLine.
+//             }                                               // Вместо пробела можно использовать литерал \t, пример 
+//         }                                               // Console.Write(matrix[i, j]+ "\t"); 
+//         Console.WriteLine();
+//     }
 // }
 // Matrix(rovs, columns);
 // Console.WriteLine();
@@ -159,7 +159,7 @@
 //    {
 
 //    matrix[i, j]= Math.Pow(matrix[i, j], 2);
-   
+
 //    }
 //                                                  // чтобы получить таблицу, нужно использовать оператор Write
 //    Console.Write(matrix[i, j]+ " ");               // вместо WriteLine.
@@ -172,3 +172,99 @@
 // Console.WriteLine();
 // MatrixSquare(rovs, columns);
 //____________________________________________________________________________________________________________________
+// 
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+//
+// Console.WriteLine($"Введите количество строк и столбцов матрицы");
+// int rovs = Convert.ToInt32(Console.ReadLine());
+// int columns = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
+
+// int[,] matrix = new int[rovs, columns];
+
+// void Matrix(int rovs, int columns)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {                                                   // для двумерных массивов используют цикл for, а вместо Length
+//         for (int j = 0; j < matrix.GetLength(1); j++)   // применяют GetLength. for(int j = 0; j < matrix.GetLength(1); i++)                                  
+//         {
+//             matrix[i, j] = new Random().Next(1, 5);
+
+//             //Console.Write(matrix[i, j]+ "\t");         // чтобы получить таблицу, нужно использовать оператор Write
+//             Console.Write(matrix[i, j] + " ");           // вместо WriteLine.
+//                                                          // вместо WriteLine.
+//         }                                                // Вместо пробела можно использовать литерал \t, пример 
+//                                                       // Console.Write(matrix[i, j]+ "\t"); 
+//       Console.WriteLine();
+      
+//     }
+    
+// }
+// //Matrix(rovs, columns);
+
+
+// void SAMatrix(int rovs, int columns)
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         //int count = 0;
+//         double sum = 0;
+//         for (int i = 0; i < matrix.GetLength(0); i++)
+//         {
+
+//             {
+//                 sum += matrix[i, j];
+//                 //count++;
+//             }
+//         }
+//         //Console.WriteLine();
+
+//         Console.WriteLine($"Среднее арифметическое столбца {j} = {sum / columns+"\t"}");
+
+//     }
+
+// }
+// Matrix(rovs, columns);
+// Console.WriteLine("_________________________________________");
+// SAMatrix(rovs, columns);
+// Console.WriteLine("_________________________________________");
+
+//____________________________________________________________________________________________________________________
+// Задача 52. Задайте двумерный массив из вещественных чисел чисел. Найдите среднее арифметическое элементов 
+// в каждом столбце.
+//
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+// Random random = new Random();
+// int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
+// double[,] arr = new double[3,4];
+// for (int i = 0; i < arr.GetLength(0); i++)
+// {
+//     for (int j = 0; j < arr.GetLength(1); j++)
+//     {
+//         arr[i, j] = new Random().NextDouble()*100;      // Таким образом конвертируем оператор(функцию) 
+//                                                         // new Random().Next() в double.
+//         Console.Write(Math.Round(arr[i, j], 1 )+ " ");
+//     }
+//     Console.WriteLine();
+// }
+// Console.WriteLine();
+// for (int j = 0; j < arr.GetLength(1); j++)
+// {
+//     double sum = 0;
+//     for (int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         sum += arr[i, j];
+//     }
+//     Console.Write($"{ sum / arr.GetLength(1)} ");
+// }
+// Console.WriteLine();
